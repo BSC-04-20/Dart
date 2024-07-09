fe83d9ebe96a101e2c3d4ed5b60699d76217dffc
bsc-04-20.weston <bsc-04-20.bsc-04-20@unima.ac.mw>
2024-07-09 06:57:29 +0200

    Finished working with arrays

--- Types.dart
+++ Types.dart
@@ -5,5 +5,9 @@ void main() {
 
   var List = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  /*
+  Lists use zero-based indexing, where 0 is the index of the first value and list.length - 1 is the index of the last value. 
+  You can get a list's length using the .length property and access a list's values using the subscript operator ([]):
+  */
   print(List[2]);
 }
