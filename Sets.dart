e15545aca31e51388aa3f8e433a6b83dd14155de
bsc-04-20.weston <bsc-04-20.bsc-04-20@unima.ac.mw>
2024-07-09 07:09:01 +0200

    Finished working with sets

--- Sets.dart
+++ Sets.dart
@@ -9,15 +9,15 @@ void main() {
   /*
     To create an empty set, use {} preceded by a type argument, or assign {} to a variable of type Set:
   */
-  var names = <String>{}; //empty set
+  var networking = <String>{}; //empty set
   // Set<String> names = {}; // This works, too.
   // var names = {}; // Creates a map, not a set.
 
   //Add items to an existing set using the add() or addAll() methods:
   courseCodes.add('inf221');
+  networking.addAll({'net211', 'net322'});
 
-  var networking = {'net221', 'net211'};
   courseCodes.addAll(networking);
 
-  
+  print(courseCodes);
 }
