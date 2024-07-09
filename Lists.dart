44401f0c628d0615cc1a474a658b6f4d30a9b1bd
bsc-04-20.weston <bsc-04-20.bsc-04-20@unima.ac.mw>
2024-07-09 06:58:10 +0200

    Renamed Type to List

---
+++ Lists.dart
@@ -0,0 +1,13 @@
+void main() {
+  /*
+      In Dart, arrays are List objects, so most people just call them lists.
+    */
+
+  var List = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  /*
+  Lists use zero-based indexing, where 0 is the index of the first value and list.length - 1 is the index of the last value. 
+  You can get a list's length using the .length property and access a list's values using the subscript operator ([]):
+  */
+  print(List[2]);
+}
